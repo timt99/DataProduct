eb6f81079080c5bf0e1adfa97fc206f96aed365b
Tim Tran <tran_tim@yahoo.com>
2016-02-21 16:26:25 -0800

    First commmit

--- server.R
+++ server.R
@@ -1,81 +1,196 @@
 library(gdata)
 library(shiny)
+library(ggvis)
+library(data.table)
+library(markdown)
+library(ggplot2)
+library(dplyr)
+library(RCurl)
+library(foreign)
+library(downloader)
 
 
-## Calculate geometric mean
-inflammationRisk <- function(vec1)
-{   
-  vec1 <- as.numeric(vec1)
-  vec1 <- vec1[!is.na(vec1)]
-  prod(vec1)^(1/length(vec1))
-  
-}
 
-
-shinyServer(function(input, output) 
+shinyServer(function(input, output, session) 
 {
+     
+     observeEvent(input$button, {
+     session$sendCustomMessage(type = 'testmessage', message = 'Thank you for clicking' )
+     })
+     
+       input_file <- reactive({
+         url <- "https://raw.githubusercontent.com/timt99/DataProduct/master/Input.csv"
+         filename <- "Input.csv"
+         if(!file.exists(filename)) download(url, destfile= filename)
+          } )
   
-
-      output$fileinput <- renderPrint(
-        { if(is.null(input$file)) return()
-          inFile <- input$file
-          RQdata <- read.csv(inFile$datapath)
-          
-          #print(summary(RQdata))
-          
-          #Calculate geometric mean or score
-          gm_mean <- function(x)
-          {   
-            x <- as.numeric(x)
-            x <- x[!is.na(x)]
-            prod(x)^(1/length(x))
-            
-          }
-          
-          
-          # Calculate the mean
-          meanRQ <- function(x)
-          {  x <- as.numeric(x)
-             x <- x[!is.na(x)]
-             mean(x)
-             
-          }
-          
-          #Predict inflammation based on score
-          predict <- function(n)
-          {
-             ifelse(n > 2, "inflame", "noninflame")
-            
-          }
-          
-          RQdata$CRMscore <- apply(RQdata[, 4:14], 1, gm_mean)
-          pred <- predict(RQdata$CRMscore)
-          RQmean <- apply(RQdata[, 4:14], 2, meanRQ)
-          header <- c("ID", "Sample Name", "18s", "CD6", "BASP1", "CXCL10", "CXCL9", "INPP5D", "ISG20", "LCK", "NKG7", "PSMB9", "RUNX3", "TAP1")
-          header2 <- c("CD6", "BASP1", "CXCL10", "CXCL9", "INPP5D", "ISG20", "LCK", "NKG7", "PSMB9", "RUNX3", "TAP1")
-          
-          indx <- sapply(RQdata, is.factor)
-          RQdata[indx] <- lapply(RQdata[indx], as.character)
-          DF1 <- rbind(header, RQdata)
-          #report <- cbind(DF1, pred)
-          DF1[] <- lapply(DF1, type.convert)
-          write.csv(DF1, "newRQ.csv")
-          print(RQdata$CRMscore)
-          sample <- RQdata[,2]
-          newdata <- rbind(sample, RQdata$CRMscore)
-          newdata2 <- rbind(newdata, pred)
-          print(newdata2)
-          DF2 <- rbind(header2, RQmean)
-          print(DF2)
-          highRQ <- max(RQmean)
-          print("Max Fold change:")
-          print(highRQ)
-          
-          
-          
+       
+      #Loading the selected file.
+       filedata <- reactive({
+       
+           infile <- input$datafile
+           if(is.null(infile)){
+             return(NULL)
+           }
+               
+             read.csv(infile$datapath)
+      
         })
+
+      
+      filedata2 <- reactive({
+        infile <- input$outfile
+        if(is.null(infile)){
+          return(NULL)
+        }
+        read.csv(infile$datapath)
+          
+      })
       
        
+       #The checkbox selector is used to determine whether we want an optional column
+       output$amountflag <- renderUI({
+         df <-filedata()
+         if (is.null(df)) return(NULL)
+         
+         checkboxInput("amountflag", "Use values?", FALSE)
+       })
+       
+       #If we do want the optional column, this is where it gets created
+       output$amountCol <- renderUI({
+         df <-filedata()
+         if (is.null(df)) return(NULL)
+         #Let's only show numeric columns
+         nums <- sapply(df, is.numeric)
+         items=names(nums[nums])
+         names(items)=items
+         selectInput("amount", "Amount:",items)
+       })
+       
+       
+        
+           output$fileData <- renderPrint({
+              input_file()
+              #read.csv("Input.csv")
+              print("Input file is downloaded to working directory")
+           })
+          
+       # This previews the CSV data file
+         output$filetable <- renderTable({
+             filedata()
+          })
+       
+       
+          output$dataTable <- renderTable({
+            filedata2()
+          })
+       
+       
+       output$fileinput <- renderPrint( 
+         { 
+           
+           
+           
+           if(is.null(input$datafile)) return()
+           inFile <- input$datafile
+           RQdata <- read.csv(inFile$datapath)
+           
+           
+             
+           #Calculate geometric mean or score
+           gm_mean <- function(x)
+           {   
+             x <- as.numeric(x)
+             x <- x[!is.na(x)]
+             prod(x)^(1/length(x))
+             
+             
+           }
+           
+           
+           # Calculate the mean
+           meanRQ <- function(x)
+           {  x <- as.numeric(x)
+           x <- x[!is.na(x)]
+           mean(x)
+           
+           }
+           
+           #Predict inflammation based on score
+           predict <- function(n)
+           {
+             ifelse(n > 2, "inflame", "noninflame")
+             
+           }
+            
+            
+                RQdata$CRMscore <- apply(RQdata[, 2:12], 1, gm_mean)
+                
+                newdata2 <- select(RQdata, Sample, CD6:CRMscore)
+               # print(newdata2)
+              
+                prediction <- predict(newdata2$CRMscore)
+                newdata3 <- cbind(newdata2, prediction)
+                newdata4 <- select(newdata3, Sample, CRMscore, prediction)
+                print(newdata4)
+                
+                RQMean <- apply(newdata2[, 2:12], 2, meanRQ)
+                print("Mean Expression of 11 genes:")
+                print(t(RQMean))
+                 
+              
+                 sortMean <- sort(RQMean)
+                 print("Rank of Gene by Expression:")
+                 print(sortMean)
+                 write.csv(newdata3, "outputRQ.csv")
+                 
+                 data_mean2 <- t(RQdata)
+                 data_mean3 <- as.data.frame(data_mean2)
+                 data_slice <- slice(data_mean3, 2:12)
+              
+                  header <- c("CD6", "BASP1", "CXCL10", "CXCL9", "INPP5D", "ISG20", "LCK", "NKG7", "PSMB9", "RUNX3", "TAP1")
+                  final_data <- cbind(header, data_slice)
+                 
+                   print(final_data)
+                  # final_data$Means <- apply(final_data[, 2:20], 1, meanRQ )
+                    final_data$Means <- apply(final_data, 1, meanRQ )
+                  
+                 
+                 output$Histogram <- renderPlot(
+                   {
+                     hist(RQdata$CRMscore, col = "green")
+                 }
+                 )
+                 
+                 output$`Prediction Result` <- renderPlot(
+                   { 
+                     g <- ggplot(data= newdata3, aes(x=Sample, y=CRMscore, fill = CRMscore))
+                     p <- g + geom_bar(colour= "blue", stat= "identity")+ labs(title = "Prediction Results")+labs(x= "Patients")+ labs(y= "CRMScore") + theme_bw()+ theme_set(theme_grey(base_size= 18)) + theme(axis.text=element_text(size=12),
+                                                                                                                                                                                                                axis.title=element_text(size=18,face="bold"))
+                     
+                     print(p)
+                     
+                      }
+                 )
+                 
+                 output$Plot3 <- renderPlot(
+                   {
+                     #hist(RQMean)
+                     g <- ggplot(final_data, aes(x = header, y= Means, fill= Means))
+                     p2 <- g + geom_bar(colour= "red", stat= "identity") + labs(title = "Average Fold Change") + labs( x= "Genes") + labs( y = "Mean Fold Change")
+                     
+                     print(p2)
+                   }
+                 )
+                 
+      }
+           
       
+      )
+       
+       
   }
 )
+      
+        
+  
--- ui.R
+++ ui.R
@@ -1,31 +1,69 @@
 library(shiny)
 
-shinyUI(pageWithSidebar(
+shinyUI(fluidPage(
   
-  headerPanel("Kidney Inflammatory prediction using a panel of expressed genes.
-               Kidney inflammatory is determined by geometric mean score.\n
-               Criteria: if score >= 2, then inflammation,
-                         else score <= 2, noninflammation\n"),
-  sidebarPanel(
-    
-    fileInput("file",'\n You can input a CSV file', accept = c('text/csv', 'text/comma-separated-values, text/plain')),
+   headerPanel("Urine Kidney Inflammatory Prediction Test using a panel of 11 expressed genes" ),
 
-    dateInput("date", "Date:")
-  ),
+    sidebarPanel(
+    # Selector for file upload
+       h4("Choose All files option after clicking Choose File below", style= "color:red"),
+       fileInput("datafile",'\n You can input a CSV file.Choose All files option to see the input file.  Input file is downloaded into local working directory', accept = c('text/csv', 'text/comma-separated-values, text/plain')),
+       actionButton("button", "Input file can be found in working directory"),
+       actionLink("button", "https://www.dropbox.com/s/hndqau1h64aq5ym/Input.csv?dl=0"),
+       tableOutput("fileData"),
+       tableOutput("fileData2"),
+       uiOutput("amountFlag"),
+       fileInput("outfile", 'Open Output file.'),
+      conditionalPanel(
+       condition= "input.amountflag==true",
+       uiOutput("amountCol")
+          ),
+             dateInput("date", "Date:")
+      
+        ),
+   
   
   mainPanel(
-    p("This is a test to predict kidney transplant inflammation using 11 gene expression values.", style="color:blue"),
-    p("The user will enter a CSV file containing gene expression values above 0 and the application will calculate the score to predict kidney inflammation.
-      ,", style= "color:blue"),
-    p("The input file is a .CSV file and will be in the same directory as the application,", style= "color:blue"),
-    h3('Results of prediction'),
-    h4('Which resulted in prediction scores below:\n'),
-    verbatimTextOutput("prediction"),
-    verbatimTextOutput("fileinput"),
-    h4('File is written to the working directory called newRQ.csv'),
-    h4('Please go to working directory using getwd() and open it now.')
     
-  )
-))
+      tableOutput("filetable"),
+      tableOutput("dataTable"),
+      h3("This is a test to predict kidney transplant inflammation using 11 gene expression values.", style="color:blue"),
+      h4("The user will enter a CSV file containing gene expression values above 0 and the application will calculate the score to predict kidney inflammation.
+        ,", style= "color:blue"),
+      h4("Input file is a .CSV file and must be saved in the same working directory as the server.R for Shiny app to run,", style= "color:blue"),
+      h3('Results of prediction'),
+      h4('Which resulted in prediction scores below:\n'),
+      verbatimTextOutput("prediction"),
+      verbatimTextOutput("fileinput"),
+      h4('File is written to the working directory called newRQ.csv', style= "color:blue"),
+      h4('Please go to working directory using getwd() and open it now.', style="color:blue"),
+      tabsetPanel(
+        tabPanel("Plot1", plotOutput("Prediction Result")),
+        tabPanel("Plot2", plotOutput("Histogram")),
+        tabPanel("Plot3", plotOutput("Plot3")),
+        tabPanel("Documentation",
+                 mainPanel(
+                   h4("This is a shiny application that input 11 genes and predict kidney inflammation by calculating the geometric mean called CRM score.  Kidney inflammatory is determined by the geometric mean score.
+                      Criteria: if score >= 2, then inflammation,
+                      else score <= 2, noninflammation
+                      The Input.csv file will be downloaded into their local working directory where the UI.R and server.R files are also located. The input file can be found here https://raw.githubusercontent.com/timt99/DataProduct/master/Input.csv
+                      or it can be download here in box: https://www.dropbox.com/s/hndqau1h64aq5ym/Input.csv?dl=0
+                      The Input.csv, the UI.R, and server.R files must all be in same working directory for the apps to work. 
+                      The user then upload this input file using the Shiny app and the Shiny app will determine whether the patients has kidney inflammation or noninflammation. 
+                      The apps predict if patient has kidny inflammation or not. The plot1 show the level of CRM scores among the patients.  Plot2 show histogram of the score among patients.
+                      Plot3 shows the average fold change for each genes among all the patients. 
+                      ", style= "color:blue")
+            )
+        )
+      
+     )
+   )
+  
+)
+)
+
+
+
+
 
 
